7638fda065abf709d9edd346b1a75d08faa5d515
AndreasPaterakis <andreas_pat@hotmail.gr>
2024-09-11 16:40:25 +0300

    First commit

---
+++ src/main/java/com/apaterakis/resttransactionapi/ResttransactionapiApplication.java
@@ -0,0 +1,13 @@
+package com.apaterakis.resttransactionapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResttransactionapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResttransactionapiApplication.class, args);
+	}
+
+}
